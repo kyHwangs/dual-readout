f9dafc08525f17d46c49e2919ab82e0aa984056a
kyHwangs <kyuyeong.hwang@gmail.com>
2026-01-12 23:42:04 +0900

    minor fix

--- analysis/make_script.cc
+++ analysis/make_script.cc
@@ -62,22 +62,26 @@ int main(int argc, char* argv[]) {
   
   std::string fCondorExcutable = R"(#! /bin/sh
 cd )" + fBaseDir + R"(
-export PATH=/cvmfs/sft.cern.ch/lcg/contrib/CMake/3.14.2/Linux-x86_64/bin:$PATH
-source /cvmfs/sft.cern.ch/lcg/contrib/gcc/8/x86_64-centos7/setup.sh
 
-source /cvmfs/sft.cern.ch/lcg/releases/LCG_96b/ROOT/6.18.04/x86_64-centos7-gcc8-opt/ROOT-env.sh
-source /cvmfs/geant4.cern.ch/geant4/10.5.p01/x86_64-centos7-gcc8-opt-MT/CMake-setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_107/x86_64-el9-gcc13-opt/setup.sh
 
-export HEPMC_DIR=/cvmfs/sft.cern.ch/lcg/releases/LCG_96b/hepmc3/3.1.2/x86_64-centos7-gcc8-opt
-export FASTJET_DIR=/cvmfs/sft.cern.ch/lcg/releases/LCG_96b/fastjet/3.3.2/x86_64-centos7-gcc8-opt
-export PYTHIA_DIR=/cvmfs/sft.cern.ch/lcg/releases/LCG_96b/MCGenerators/pythia8/240/x86_64-centos7-gcc8-opt
+export HEPMC_DIR=/cvmfs/sft.cern.ch/lcg/releases/hepmc3/3.2.7-cd228/x86_64-el9-gcc13-opt
+export FASTJET_DIR=/cvmfs/sft.cern.ch/lcg/releases/fastjet/3.4.3-7b8bd/x86_64-el9-gcc13-opt
+export PYTHIA_DIR=/cvmfs/sft.cern.ch/lcg/releases/MCGenerators/pythia8/313-ba28f/x86_64-el9-gcc13-opt
 
-export PYTHIA8=/cvmfs/sft.cern.ch/lcg/releases/LCG_96b/MCGenerators/pythia8/240/x86_64-centos7-gcc8-opt
-export PYTHIA8DATA=/cvmfs/sft.cern.ch/lcg/releases/LCG_96b/MCGenerators/pythia8/240/x86_64-centos7-gcc8-opt/share/Pythia8/xmldoc
-export ROOT_INCLUDE_PATH=/cvmfs/sft.cern.ch/lcg/releases/LCG_96b/hepmc3/3.1.2/x86_64-centos7-gcc8-opt/include:$ROOT_INCLUDE_PATH
+export PYTHIA8=/cvmfs/sft.cern.ch/lcg/releases/MCGenerators/pythia8/313-ba28f/x86_64-el9-gcc13-opt
+export PYTHIA8DATA=/cvmfs/sft.cern.ch/lcg/releases/MCGenerators/pythia8/313-ba28f/x86_64-el9-gcc13-opt/share/Pythia8/xmldoc
+export ROOT_INCLUDE_PATH=/cvmfs/sft.cern.ch/lcg/releases/ROOT/6.34.02-18eb6/x86_64-el9-gcc13-opt/include:$ROOT_INCLUDE_PATH
 
 export DRCBASE=`pwd`
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HEPMC_DIR/lib64:$FASTJET_DIR/lib:$PYTHIA_DIR:$PWD/install/lib
+alias build_drc='cd $DRCBASE/build; make -j4 install; cd -'
+
+export INSTALL_DIR_PATH=$PWD/install
+export PATH=$PATH:$INSTALL_DIR_PATH/lib:$INSTALL_DIR_PATH/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INSTALL_DIR_PATH/lib
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HEPMC_DIR/lib64:$FASTJET_DIR/lib:$PYTHIA_DIR/lib
+
 cd )" + fConfigBaseDir + R"(
 
 )" + fInstallBaseDir + R"(/bin/DRsim \
@@ -101,7 +105,7 @@ arguments           = $(ProcId)
 output              = )" + fLogBaseDir + R"(/out_$(ProcId).out
 error               = )" + fLogBaseDir + R"(/err_$(ProcId).err
 log                 = )" + fLogBaseDir + R"(/log_$(ProcId).log
-request_memory      = 1.5 GB
+request_memory      = 3 GB
 
 should_transfer_files = YES
 when_to_transfer_output = ON_EXIT
